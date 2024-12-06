6257f89c4cfc74ec407a67ef74f307f2ccb4b7e0
WouterDobber <72268241+WouterDobber@users.noreply.github.com>
2024-12-06 11:01:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+# Sidebar button
+if st.sidebar.button("README"):
+    with st.sidebar:
+        st.write("### README")
+        st.write("Here is some random text to display when the button is clicked!")
