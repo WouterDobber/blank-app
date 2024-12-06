dced91856cb2e7384f452d35294f1f64d29fa156
WouterDobber <72268241+WouterDobber@users.noreply.github.com>
2024-12-06 11:03:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,30 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# Add custom CSS to position the button at the bottom left
+st.markdown(
+    """
+    <style>
+    .custom-button {
+        position: fixed;
+        bottom: 10px;
+        left: 10px;
+        z-index: 9999;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
 )
 
-# Sidebar button
-if st.sidebar.button("README"):
-    with st.sidebar:
-        st.write("### README")
-        st.write("Here is some random text to display when the button is clicked!")
+# Display the button
+if st.button("README", key="readme"):
+    st.session_state["show_modal"] = True
+else:
+    st.session_state["show_modal"] = False
+
+# If the button is pressed, show a modal window
+if st.session_state.get("show_modal", False):
+    st.write("""
+    **Modal Window:**
+    Here is some random text to display when the button is clicked!
+    """)
+
